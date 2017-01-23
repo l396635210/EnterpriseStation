8ab294b72402fdad34323f3b2ed6b4c828e9ca0d
Lizheng <396635210@qq.com>
2017-01-24 06:09:07 +0800

    2017年1月24日 06:09:06

--- src/AppBundle/Admin/CategoryAdmin.php
+++ src/AppBundle/Admin/CategoryAdmin.php
@@ -1,2 +1,43 @@
 <?php
 
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', 'text',[
+                    'label' => '标题',
+                ])
+                ->add('parent', 'sonata_type_model', [
+                    'label' => '父栏目',
+                    'required' => false,
+                ])
+                ->add('status', 'checkbox', [
+                    'label' => '是否有效',
+                ])
+                ->add('data', 'textarea', [
+                    'label' => '其他数据',
+                    'required' => false,
+                ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name')
+            ->add('parent')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('parent')
+            ->addIdentifier('name')
+        ;
+    }
+}
